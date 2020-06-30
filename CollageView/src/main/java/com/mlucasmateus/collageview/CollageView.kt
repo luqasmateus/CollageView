b1d2f0ab7b1e10b217f5bb94a89957d255f76a95
mlucasmateus <mlucasmateus@gmail.com>
2020-06-29 21:52:46 -0300

    Update CollageView.kt

--- CollageView/src/main/java/com/mlucasmateus/collageview/CollageView.kt
+++ CollageView/src/main/java/com/mlucasmateus/collageview/CollageView.kt
@@ -1,9 +1,12 @@
 package com.mlucasmateus.collageview
 
 import android.content.Context
+import android.util.AttributeSet
 import android.widget.FrameLayout
 
-class CollageView(context: Context) : FrameLayout(context) {
+abstract class CollageView(context: Context, var attributes : Attributes) : FrameLayout(context) {
 
-    
+    open class Attributes
+    open class Builder
+    open class CustomSpec
 }
